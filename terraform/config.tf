21f04e317890829668d283b991fdcaf720f42a07
Arthur Laurent <arthur.laurent@wifirst.fr>
2020-08-11 14:08:15 +0200

    Add DNS domain creation through OVH

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,6 +1,9 @@
 provider "digitalocean" {
 }
 
+provider "ovh" {
+}
+
 resource "digitalocean_kubernetes_cluster" "my_cluster" {
   name    = "my-cluster"
   region  = "lon1"
@@ -26,6 +29,32 @@ provider "kubernetes" {
 resource "local_file" "kube_config" {
   content = digitalocean_kubernetes_cluster.my_cluster.kube_config[0].raw_config
   filename = "${path.module}/kube_config"
+}
+
+variable "main_domain" {}
+
+resource "ovh_domain_zone_record" "main_domain" {
+    zone = var.main_domain
+    subdomain = ""
+    fieldtype = "A"
+    ttl = "3600"
+    target = digitalocean_kubernetes_cluster.my_cluster.ipv4_address
+}
+
+resource "ovh_domain_zone_record" "main_subdomains" {
+    zone = var.main_domain
+    subdomain = "*"
+    fieldtype = "A"
+    ttl = "3600"
+    target = digitalocean_kubernetes_cluster.my_cluster.ipv4_address
+}
+
+resource "ovh_domain_zone_record" "dev_subdomains" {
+    zone = var.main_domain
+    subdomain = "*.dev"
+    fieldtype = "A"
+    ttl = "3600"
+    target = digitalocean_kubernetes_cluster.my_cluster.ipv4_address
 }
 
 output "public_ip" {

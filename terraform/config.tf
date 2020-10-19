fa7557cd49c1f6bc887b513a19b11041b8320b05
Arthur Laurent <arthur.laurent@wifirst.fr>
2020-10-19 02:15:46 +0200

    Fix indentation

--- terraform/config.tf
+++ terraform/config.tf
@@ -5,25 +5,26 @@ provider "ovh" {
 }
 
 resource "digitalocean_kubernetes_cluster" "my_cluster" {
-  name    = "my-cluster"
-  region  = "lon1"
-  version = "1.18.6-do.0"
-  tags    = []
+    name    = "my-cluster"
+    region  = "lon1"
+    version = "1.18.6-do.0"
+    tags    = []
 
-  node_pool {
-    name       = "worker-pool"
-    size       = "s-2vcpu-2gb"
-    node_count = 1
-  }
+    node_pool {
+        name       = "worker-pool"
+        size       = "s-2vcpu-2gb"
+        node_count = 1
+    }
 }
 
 provider "kubernetes" {
-  load_config_file = false
-  host  = digitalocean_kubernetes_cluster.my_cluster.endpoint
-  token = digitalocean_kubernetes_cluster.my_cluster.kube_config[0].token
-  cluster_ca_certificate = base64decode(
-    digitalocean_kubernetes_cluster.my_cluster.kube_config[0].cluster_ca_certificate
-  )
+    load_config_file = false
+    host  = digitalocean_kubernetes_cluster.my_cluster.endpoint
+    token = digitalocean_kubernetes_cluster.my_cluster.kube_config[0].token
+
+    cluster_ca_certificate = base64decode(
+        digitalocean_kubernetes_cluster.my_cluster.kube_config[0].cluster_ca_certificate
+    )
 }
 
 resource "digitalocean_loadbalancer" "public" {
@@ -53,8 +54,8 @@ resource "digitalocean_loadbalancer" "public" {
 }
 
 resource "local_file" "kube_config" {
-  content = digitalocean_kubernetes_cluster.my_cluster.kube_config[0].raw_config
-  filename = "${path.module}/kube_config"
+    content = digitalocean_kubernetes_cluster.my_cluster.kube_config[0].raw_config
+    filename = "${path.module}/kube_config"
 }
 
 variable "main_domain" {}
